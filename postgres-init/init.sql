9554acea92620ce48d3ee1f3fab7430105692907
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2026-03-18 17:48:52 +0000

    chore: adiciona usuários isolados por banco no PostgreSQL
    - Criado usuário dedicado para cada banco (auth_user, companies_user, drivers_user, cargos_user, operation_user)
    - Permissões restritas ao próprio banco para cada usuário

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,5 +1,20 @@
+-- Bancos
 CREATE DATABASE auth_db;
 CREATE DATABASE companies_db;
 CREATE DATABASE drivers_db;
 CREATE DATABASE cargos_db;
 CREATE DATABASE operation_db;
+
+-- Usuários isolados
+CREATE USER auth_user WITH PASSWORD 'auth123';
+CREATE USER companies_user WITH PASSWORD 'companies123';
+CREATE USER drivers_user WITH PASSWORD 'drivers123';
+CREATE USER cargos_user WITH PASSWORD 'cargos123';
+CREATE USER operation_user WITH PASSWORD 'operation123';
+
+-- Permissões restritas (cada usuário só acessa seu banco)
+GRANT ALL PRIVILEGES ON DATABASE auth_db TO auth_user;
+GRANT ALL PRIVILEGES ON DATABASE companies_db TO companies_user;
+GRANT ALL PRIVILEGES ON DATABASE drivers_db TO drivers_user;
+GRANT ALL PRIVILEGES ON DATABASE cargos_db TO cargos_user;
+GRANT ALL PRIVILEGES ON DATABASE operation_db TO operation_user;
