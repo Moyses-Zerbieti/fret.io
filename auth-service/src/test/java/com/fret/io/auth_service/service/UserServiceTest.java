9f6af605a3b877884b5d0389200d880b347c881b
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2026-04-17 19:21:18 -0300

    test: adiciona testes unitários para UserService
    Testes do método registerUser (CPF, CNPJ e documento inválido)
    Testes do método loginUser (sucesso, usuário não encontrado, usuário inativo, senha inválida)
    Testes do método updateStatusUser (alteração de status, revogação de tokens, publicação de evento, usuário não encontrado)

---
+++ auth-service/src/test/java/com/fret/io/auth_service/service/UserServiceTest.java
@@ -0,0 +1,232 @@
+package com.fret.io.auth_service.service;
+
+import com.fret.io.auth_service.config.PasswordValidator;
+import com.fret.io.auth_service.dto.LoginRequest;
+import com.fret.io.auth_service.dto.RegisterRequest;
+import com.fret.io.auth_service.exception.DocInvalidException;
+import com.fret.io.auth_service.exception.UserInactiveException;
+import com.fret.io.auth_service.model.DocumentType;
+import com.fret.io.auth_service.model.Role;
+import com.fret.io.auth_service.model.User;
+import com.fret.io.auth_service.model.UserStatus;
+import com.fret.io.auth_service.repository.RefreshTokenRepository;
+import com.fret.io.auth_service.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    PasswordEncoder encoder;
+    @Mock
+    PasswordValidator passwordValidator;
+    @Mock
+    UserEventPublisher eventPublisher;
+    @Mock
+    RefreshTokenRepository refreshTokenRepository;
+
+    @Test
+    void registerUserWithCpfTest(){
+        RegisterRequest request = new RegisterRequest();
+        request.setDocument("123.456.789-01");
+        request.setPassword("123456");
+        request.setEmail("email@test.com");
+
+        when(encoder.encode("123456")).thenReturn("hashedPassword");
+
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        doNothing().when(passwordValidator).validatePassword(any());
+
+        userService.registerUser(request);
+
+        verify(passwordValidator).validatePassword("123456");
+
+        verify(userRepository).save(argThat(user ->
+                user.getRole() == Role.MOTORISTA &&
+                user.getEmail().equals("email@test.com")&&
+                user.getPasswordHash().equals("hashedPassword")
+        ));
+
+        verify(eventPublisher).publishUserRegistered(any(User.class));
+    }
+
+    @Test
+    void registerUserWithCnpjTest(){
+        RegisterRequest request = new RegisterRequest();
+        request.setDocument("12.345.678/9101-10");
+        request.setEmail("test@gmail.com");
+        request.setPassword("123456");
+
+        when(encoder.encode("123456")).thenReturn("hashedPassword");
+
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        doNothing().when(passwordValidator).validatePassword("123456");
+
+        userService.registerUser(request);
+
+        verify(userRepository).save(argThat(user ->
+                user.getRole() == Role.EMBARCADORA &&
+                user.getDocumentType() == DocumentType.CNPJ &&
+                user.getEmail().equals("test@gmail.com") &&
+                user.getPasswordHash().equals("hashedPassword")
+        ));
+
+        verify(eventPublisher).publishUserRegistered(any(User.class));
+    }
+
+    @Test
+    void registerUserWithCpfExceptionTest(){
+        RegisterRequest request = new RegisterRequest();
+        request.setDocument("12345678901");
+        request.setPassword("123456");
+
+        doNothing().when(passwordValidator).validatePassword(any());
+
+        assertThrows(DocInvalidException.class, () ->{
+            userService.registerUser(request);
+        });
+
+        verify(userRepository, never()).save(any());
+        verify(eventPublisher, never()).publishUserRegistered(any());
+    }
+
+    @Test
+    void resgisterUserWithCnpjExceptionTest(){
+        RegisterRequest request = new RegisterRequest();
+        request.setDocument("12345678901");
+        request.setPassword("123456");
+
+        doNothing().when(passwordValidator).validatePassword(any());
+
+        assertThrows(DocInvalidException.class, () ->{
+            userService.registerUser(request);
+        });
+
+        verify(userRepository, never()).save(any());
+        verify(eventPublisher, never()).publishUserRegistered(any());
+    }
+
+    @Test
+    void loginSucessTest(){
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("test@gmail.com");
+        loginRequest.setPassword("123456");
+
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setPasswordHash("hashedPassword");
+        user.setUserStatus(UserStatus.ACTIVE);
+
+        when(userRepository.findByEmail("test@gmail.com"))
+                .thenReturn(Optional.of(user));
+
+        when(encoder.matches("123456", "hashedPassword"))
+                .thenReturn(true);
+
+        User result = userService.loginUser(loginRequest);
+
+        assertEquals(user, result);
+
+        verify(userRepository).findByEmail("test@gmail.com");
+        verify(encoder).matches("123456", "hashedPassword");
+    }
+
+    @Test
+    void loginStatusBlockedOrSuspended(){
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("test@gmail.com");
+        loginRequest.setPassword("123456");
+
+        User user  = new User();
+        user.setEmail("test@gmail.com");
+        user.setPasswordHash("hashedPassord");
+        user.setUserStatus(UserStatus.BLOCKED);
+
+        when(userRepository.findByEmail("test@gmail.com"))
+                .thenReturn(Optional.of(user));
+
+        assertThrows(UserInactiveException.class, ()-> {
+            userService.loginUser(loginRequest);
+
+        });
+
+        verify(userRepository).findByEmail("test@gmail.com");;
+        verify(encoder, never()).matches(any(), any());
+    }
+
+    @Test
+    void loginWithPasswordInvalidExeption(){
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("test@gmail.com");
+        loginRequest.setPassword("123456");
+
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setPasswordHash("hashedPassword");
+        user.setUserStatus(UserStatus.ACTIVE);
+
+        when(userRepository.findByEmail("test@gmail.com"))
+                .thenReturn(Optional.of(user));
+
+        when(encoder.matches("123456", "hashedPassword"))
+                .thenReturn(false);
+
+        assertThrows(RuntimeException.class, ()->{
+            userService.loginUser(loginRequest);
+
+        });
+
+        verify(userRepository).findByEmail("test@gmail.com");
+        verify(encoder).matches("123456", "hashedPassword");
+    }
+
+    @Test
+    void changeUserStatusForBlockedOrSuspendedTest(){
+        UUID userId = UUID.randomUUID();
+
+        User user = new User();
+        user.setId(userId);
+        user.setUserStatus(UserStatus.ACTIVE);
+
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.of(user));
+
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        doNothing().when(refreshTokenRepository).revokeAllByUserId(userId);
+
+        userService.updateStatusUser(userId, UserStatus.BLOCKED);
+
+        verify(userRepository).save(argThat(updateUser ->
+                updateUser.getUserStatus() == UserStatus.BLOCKED &&
+                updateUser.getUpdatedAt() != null
+
+        ));
+
+        verify(refreshTokenRepository).revokeAllByUserId(userId);
+        verify(eventPublisher).publishUserDeactivated(userId, "BLOCKED");
+    }
+}
