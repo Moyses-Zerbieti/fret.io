10af48f3221376fc722e8d81b3813c353fc69dcc
Moyses-Zerbieti <zerbietimoyses@gmail.com>
2026-07-18 15:04:53 -0300

    feat/driver-cnh-validation: Implementa validacao de CNH e atualizacao do numero da CNH do motorista
    - Adiciona CnhValidator para validar formato da CNH com 11 digitos numericos
    - Normaliza o numero da CNH antes de salvar os dados
    - Implementa validacao de CNH no completeRegistration
    - Permite atualizacao do numero da CNH no updateDriver
    - Adiciona verificacao de duplicidade de CNH entre motoristas
    - Evita conflito ao atualizar a propria CNH do motorista

--- driver-service/src/main/java/com/fret/io/driver_service/exception/CnhAlreadyRegisteredException.java
+++ driver-service/src/main/java/com/fret/io/driver_service/exception/CnhAlreadyRegisteredException.java
@@ -3,6 +3,6 @@ package com.fret.io.driver_service.exception;
 public class CnhAlreadyRegisteredException extends RuntimeException {
 
     public CnhAlreadyRegisteredException(){
-        super("Número de CNH informado já cadastrado no sistema");
+        super("Número de CNH informado já existe no sistema");
     }
 }
